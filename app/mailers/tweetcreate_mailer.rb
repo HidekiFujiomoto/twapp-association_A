3e2eea1abb0ee7c37f980272fcce0fd95a8892ef
HidekiFujimoto <kurosirobeagle@yahoo.co.jp>
2018-04-27 11:56:11 +0900

    ActionMailer改修B

--- app/mailers/tweetcreate_mailer.rb
+++ app/mailers/tweetcreate_mailer.rb
@@ -2,7 +2,7 @@ class TweetcreateMailer < ApplicationMailer
   def send_email(user_email)
 
     @user_email = user_email
-    
-    mail to: user_email, subject: "つぶやきを投稿しました"
+
+    mail to: user_email, subject: "新規つぶやきを投稿しました"
   end
 end
